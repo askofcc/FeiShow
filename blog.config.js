1a44eb2cf27a04fef961789c03fa7fb42b5e4bed
askofcc <996280254@qq.com>
2026-07-27 21:42:34 +0800

    fix: default CUSTOM_MENU off unless explicitly enabled

--- blog.config.js
+++ blog.config.js
@@ -59,7 +59,8 @@ const BLOG = {
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 自定义菜单
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu类型的菜单，替代了3.12版本前的Page类型
+  // FeishuNext: default OFF. Only on when env is explicit 'true' OR CONFIG-TABLE enables CUSTOM_MENU.
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU === 'true', // 内容表菜单/子菜单；默认关
 
   // 文章列表相关设置
   CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
